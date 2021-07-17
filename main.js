35a32ce498f7963b5532c628ea549dd665006329
rainealexander <raine@rainealexander.dev>
2021-07-16 20:24:28 -0500

    Initial setup & commit

---
+++ main.js
@@ -0,0 +1,8 @@
+// ==================================
+// |  Procedural Art in Javascript  |
+// ==================================
+
+// STEP 1: GET THE CANVAS FROM THE DOM
+// AND CREATE A 'CONTEXT' TO RENDER TO
+const canvas = document.getElementByID('myCanvas');
+const context = canvas.getContext('2d');
